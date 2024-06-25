394d2240756c0c008893462dd4d1f11b0cc67502
ryan3521 <ryan.chen.1104@gmail.com>
2024-06-25 13:37:29 +0800

    k means cluster fix point 2

--- kmeans_cls.cpp
+++ kmeans_cls.cpp
@@ -78,7 +78,6 @@ void DoCluster(double dis_delay, lib& LIB, inst& INST, vector<int>& map_to_clust
             // Find UP
             c = kcr[up_i];
             if(find_up){
-                hpwl_diff = abs(c->cen_x - f->cen_x) + abs(c->cen_y - f->cen_y);
                 if(find == true){
                     if(min_hpwl <= c->cen_x - f->cen_x){
                         find_up = false;
@@ -91,6 +90,7 @@ void DoCluster(double dis_delay, lib& LIB, inst& INST, vector<int>& map_to_clust
                 }
             }
             if(find_up){
+                hpwl_diff = abs(c->cen_x - f->cen_x) + abs(c->cen_y - f->cen_y);
                 if(f->allow_displace(c->cen_x, c->cen_y, dis_delay)==true && ((c->size)+(f->type->bit_num) <= LIB.max_ff_size)){
                     if(find == false){
                         find = true;
