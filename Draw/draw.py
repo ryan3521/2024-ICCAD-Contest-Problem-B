72e8a9ed17849b29aa1e0c080f106a1a0728314f
Ryan <ryan.chen.1104@gmail.com>
2024-12-16 23:57:49 +0800

    make commit

--- Draw/draw.py
+++ Draw/draw.py
@@ -2,120 +2,135 @@ import numpy as np
 import matplotlib.pyplot as plt
 
 
-# **********************************************************
-# *                Original FF Instances                   *
-# **********************************************************
-# f = open('ff_original.txt', 'r')
-
-# die_w, die_h = [float(x) for x in next(f).split()] # read first line
-# ff_num = [int(x) for x in next(f).split()] # read second line
-
-# scale_factor = 100/die_w
-
-# plt.figure(figsize=(die_w*scale_factor, die_h*scale_factor))
-
-# fig = plt.gcf()
-# ax = fig.gca()
-
-# for line in f:
-#     ff_name, coox, cooy, w, h = [word for word in line.split()]
-
-#     coox = float(coox)
-#     cooy = float(cooy)
-#     w = float(w)
-#     h = float(h)
-
-#     patch = plt.Rectangle(xy=(coox*scale_factor, cooy*scale_factor), fill=False, width=w*scale_factor, height=h*scale_factor, color='r')
-#     ax.add_patch(patch)
-
-# plt.xlim([0, die_w*scale_factor])
-# plt.ylim([0, die_h*scale_factor])
-
-# f = open('gate.txt', 'r')
-
-# die_w, die_h = [float(x) for x in next(f).split()] # read first line
-# ff_num = [int(x) for x in next(f).split()] # read second line
-
-# for line in f:
-#     ff_name, coox, cooy, w, h = [word for word in line.split()]
-
-#     coox = float(coox)
-#     cooy = float(cooy)
-#     w = float(w)
-#     h = float(h)
-
-#     patch = plt.Rectangle(xy=(coox*scale_factor, cooy*scale_factor), fill=False, width=w*scale_factor, height=h*scale_factor, color='g')
-#     ax.add_patch(patch)
-
-
-# plt.xlim([0, die_w*scale_factor])
-# plt.ylim([0, die_h*scale_factor])
-
-
-# plt.savefig('ffs_original.png')
-# f.close()
-
-
+drawOriginal = False
+drawBankingResult = True
+markDisplacement = True
 
 
 # **********************************************************
-# *                Placement Result                        *
+# *       Draw Original FF Instances and Comb Gates        *
 # **********************************************************
-f = open('placement result.txt', 'r')
+if drawOriginal:
+    f = open('ff_original.txt', 'r')
 
-die_w, die_h = [float(x) for x in next(f).split()] # read first line
-ff_num = [int(x) for x in next(f).split()] # read second line
+    die_w, die_h = [float(x) for x in next(f).split()] # read first line
+    ff_num = [int(x) for x in next(f).split()] # read second line
 
-scale_factor = 100/die_w
+    scale_factor = 100/die_w
 
-plt.figure(figsize=(die_w*scale_factor, die_h*scale_factor))
+    plt.figure(figsize=(die_w*scale_factor, die_h*scale_factor))
 
-fig = plt.gcf()
-ax = fig.gca()
+    fig = plt.gcf()
+    ax = fig.gca()
 
-for line in f:
-    ff_name, coox, cooy, w, h, cenx, ceny, ori_cenx, ori_ceny = [word for word in line.split()]
+    for line in f:
+        ff_name, coox, cooy, w, h = [word for word in line.split()]
 
+        coox = float(coox)
+        cooy = float(cooy)
+        w = float(w)
+        h = float(h)
 
-    coox = float(coox)
-    cooy = float(cooy)
-    w = float(w)
-    h = float(h)
-    cenx = float(cenx)*scale_factor
-    ceny = float(ceny)*scale_factor
-    ori_cenx  = float(ori_cenx)*scale_factor
-    ori_ceny  = float(ori_ceny)*scale_factor
+        patch = plt.Rectangle(xy=(coox*scale_factor, cooy*scale_factor), fill=False, width=w*scale_factor, height=h*scale_factor, color='r')
+        ax.add_patch(patch)
 
+    plt.xlim([0, die_w*scale_factor])
+    plt.ylim([0, die_h*scale_factor])
 
-    patch = plt.Rectangle(xy=(coox*scale_factor, cooy*scale_factor), fill=False, width=w*scale_factor, height=h*scale_factor, color='r')
-    ax.add_patch(patch)
+    f = open('gate.txt', 'r')
 
-    # plt.plot(cenx, ceny, 'o', color='b') # plot dot
-    # plt.plot([cenx, ori_cenx], [ceny, ori_ceny], color='b') # plot line
+    die_w, die_h = [float(x) for x in next(f).split()] # read first line
+    ff_num = [int(x) for x in next(f).split()] # read second line
 
-f.close()
+    for line in f:
+        ff_name, coox, cooy, w, h = [word for word in line.split()]
 
-f = open('gate.txt', 'r')
+        coox = float(coox)
+        cooy = float(cooy)
+        w = float(w)
+        h = float(h)
 
-die_w, die_h = [float(x) for x in next(f).split()] # read first line
-ff_num = [int(x) for x in next(f).split()] # read second line
-
-for line in f:
-    ff_name, coox, cooy, w, h = [word for word in line.split()]
-
-    coox = float(coox)
-    cooy = float(cooy)
-    w = float(w)
-    h = float(h)
-
-    patch = plt.Rectangle(xy=(coox*scale_factor, cooy*scale_factor), fill=False, width=w*scale_factor, height=h*scale_factor, color='g')
-    ax.add_patch(patch)
+        patch = plt.Rectangle(xy=(coox*scale_factor, cooy*scale_factor), fill=False, width=w*scale_factor, height=h*scale_factor, color='g')
+        ax.add_patch(patch)
 
 
-plt.xlim([0, die_w*scale_factor])
-plt.ylim([0, die_h*scale_factor])
+    plt.xlim([0, die_w*scale_factor])
+    plt.ylim([0, die_h*scale_factor])
 
-plt.savefig('placement.png')
-f.close()
+
+    plt.savefig('original.png')
+    f.close()
+
+
+
+
+# **********************************************************
+# *      Draw Banking Result (with Comb gates)             *
+# **********************************************************
+if drawBankingResult:
+    f = open('placement result.txt', 'r')
+
+    outputFileName = "bankingResult.png"
+    if markDisplacement: outputFileName = "bankingResult_markDisplace.png"
+
+    die_w, die_h = [float(x) for x in next(f).split()] # read first line
+    ff_num = [int(x) for x in next(f).split()] # read second line
+
+    scale_factor = 100/die_w
+
+    plt.figure(figsize=(die_w*scale_factor, die_h*scale_factor))
+
+    fig = plt.gcf()
+    ax = fig.gca()
+
+    for line in f:
+        ff_name, coox, cooy, w, h, cenx, ceny, ori_cenx, ori_ceny = [word for word in line.split()]
+
+
+        coox = float(coox)
+        cooy = float(cooy)
+        w = float(w)
+        h = float(h)
+        cenx = float(cenx)*scale_factor
+        ceny = float(ceny)*scale_factor
+        ori_cenx  = float(ori_cenx)*scale_factor
+        ori_ceny  = float(ori_ceny)*scale_factor
+
+
+        patch = plt.Rectangle(xy=(coox*scale_factor, cooy*scale_factor), fill=False, width=w*scale_factor, height=h*scale_factor, color='r')
+        ax.add_patch(patch)
+
+        ##############################      
+        # Draw the displacement line #
+        ##############################
+        if markDisplacement:
+            plt.plot(cenx, ceny, 'o', color='b') # plot dot
+            plt.plot([cenx, ori_cenx], [ceny, ori_ceny], color='b') # plot line
+            
+
+    f.close()
+
+    f = open('gate.txt', 'r')
+
+    die_w, die_h = [float(x) for x in next(f).split()] # read first line
+    ff_num = [int(x) for x in next(f).split()] # read second line
+
+    for line in f:
+        ff_name, coox, cooy, w, h = [word for word in line.split()]
+
+        coox = float(coox)
+        cooy = float(cooy)
+        w = float(w)
+        h = float(h)
+
+        patch = plt.Rectangle(xy=(coox*scale_factor, cooy*scale_factor), fill=False, width=w*scale_factor, height=h*scale_factor, color='g')
+        ax.add_patch(patch)
+
+
+    plt.xlim([0, die_w*scale_factor])
+    plt.ylim([0, die_h*scale_factor])
+
+    plt.savefig(outputFileName)
+    f.close()
 
 
